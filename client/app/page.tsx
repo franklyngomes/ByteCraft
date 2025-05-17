05e0970408eeba326a793073859a5e9987247a65
franklyngomes <franklyngomes15@gmail.com>
2025-05-17 15:09:44 +0530

    ByteCraft

---
+++ client/app/page.tsx
@@ -0,0 +1,266 @@
+import { clientLogo } from "./lib/data";
+import { portfolio } from "./lib/data";
+import { services } from "./lib/data";
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <section id="hero">
+        <div
+          id="heroCarousel"
+          className="carousel slide carousel-fade"
+          data-ride="carousel"
+        >
+          <div className="carousel-inner" role="listbox">
+            <div
+              className="carousel-item active"
+              style={{ backgroundImage: "url(/assets/img/slide/slide-1.jpg)" }}
+            >
+              <div className="carousel-container">
+                <div className="carousel-content animate__animated animate__fadeInUp">
+                  <h2>
+                    Welcome to <span>ByteCraft</span>
+                  </h2>
+                  <p>
+                    Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea
+                    ut et est quaerat sequi nihil ut aliquam. Occaecati alias
+                    dolorem mollitia ut. Similique ea voluptatem. Esse
+                    doloremque accusamus repellendus deleniti vel. Minus et
+                    tempore modi architecto.
+                  </p>
+                  <div className="text-center">
+                    <a href="" className="btn-get-started">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="carousel-item"
+              style={{ backgroundImage: "url(/assets/img/slide/slide-2.jpg)" }}
+            >
+              <div className="carousel-container">
+                <div className="carousel-content animate__animated animate__fadeInUp">
+                  <h2>Lorem Ipsum Dolor</h2>
+                  <p>
+                    Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea
+                    ut et est quaerat sequi nihil ut aliquam. Occaecati alias
+                    dolorem mollitia ut. Similique ea voluptatem. Esse
+                    doloremque accusamus repellendus deleniti vel. Minus et
+                    tempore modi architecto.
+                  </p>
+                  <div className="text-center">
+                    <a href="" className="btn-get-started">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="carousel-item"
+              style={{ backgroundImage: "url(/assets/img/slide/slide-3.jpg)" }}
+            >
+              <div className="carousel-container">
+                <div className="carousel-content animate__animated animate__fadeInUp">
+                  <h2>Sequi ea ut et est quaerat</h2>
+                  <p>
+                    Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea
+                    ut et est quaerat sequi nihil ut aliquam. Occaecati alias
+                    dolorem mollitia ut. Similique ea voluptatem. Esse
+                    doloremque accusamus repellendus deleniti vel. Minus et
+                    tempore modi architecto.
+                  </p>
+                  <div className="text-center">
+                    <a href="" className="btn-get-started">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <a
+            className="carousel-control-prev"
+            href="#heroCarousel"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon icofont icofont-simple-left"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+
+          <a
+            className="carousel-control-next"
+            href="#heroCarousel"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon icofont icofont-simple-right"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
+
+          {/* <ol className="carousel-indicators" id="hero-carousel-indicators"></ol> */}
+        </div>
+      </section>
+
+      <main id="main">
+        <section id="about-us" className="about-us">
+          <div className="container">
+            <div className="section-title">
+              <h2>
+                About Us
+              </h2>
+            </div>
+
+            <div className="row content">
+              <div className="col-lg-6">
+                <h2>Eum ipsam laborum deleniti velitena</h2>
+                <h3>
+                  Voluptatem dignissimos provident quasi corporis voluptates sit
+                  assum perenda sruen jonee trave
+                </h3>
+              </div>
+              <div className="col-lg-6 pt-4 pt-lg-0">
+                <p>
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                  aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum
+                </p>
+                <ul>
+                  <li>
+                    <i className="ri-check-double-line"></i> Ullamco laboris
+                    nisi ut aliquip ex ea commodo consequa
+                  </li>
+                  <li>
+                    <i className="ri-check-double-line"></i> Duis aute irure
+                    dolor in reprehenderit in voluptate velit
+                  </li>
+                  <li>
+                    <i className="ri-check-double-line"></i> Ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                    dolor in reprehenderit in
+                  </li>
+                </ul>
+                <p className="font-italic">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="services" className="services section-bg">
+          <div className="container">
+            <div className="section-title">
+              <h2>
+                Services
+              </h2>
+              <p>
+                Laborum repudiandae omnis voluptatum consequatur mollitia ea est
+                voluptas ut
+              </p>
+            </div>
+
+            <div className="row">
+              {services.map((item, index) => (
+                <div
+                  className="col-lg-4 col-md-6 d-flex align-items-stretch  my-4 mt-md-0" key={index}
+                >
+                  <div className={`icon-box ${item.iconBox}`}>
+                    <div className="icon">
+                      <div
+                        className="mb-4"
+                        dangerouslySetInnerHTML={{ __html: item.iconBg }}
+                      />
+                      <i className={`bx ${item.iconLogo}`}></i>
+                    </div>
+                    <h4>
+                      <a href="">{item.title}</a>
+                    </h4>
+                    <p>
+                      {item.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+
+            </div>
+          </div>
+        </section>
+
+        <section id="portfolio" className="portfolio">
+          <div className="container">
+            <div className="section-title">
+              <h2>Portfolio</h2>
+            </div>
+
+            <div className="row">
+              {portfolio?.map((item, index) => (
+                <div
+                  className="col-lg-4 col-md-6 portfolio-item"
+                  key={index}
+                >
+                  <img
+                    src={item.image}
+                    className="img-fluid"
+                    alt={item.title}
+                  />
+                  <div className="portfolio-info">
+                    <h4>{item.title}</h4>
+                    <p>{item.title}</p>
+                    <a
+                      href="/details"
+                      className="details-link"
+                      title="More Details"
+                    >
+                      <i className="bx bx-link"></i>
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <section id="clients" className="clients">
+          <div className="container">
+            <div className="section-title">
+              <h2>Clients</h2>
+            </div>
+
+            <div
+              className="row no-gutters clients-wrap clearfix"
+            >
+              {clientLogo.map((item) => (
+                <div className="col-lg-3 col-md-4 col-6" key={item.title}>
+                  <div className="client-logo">
+                    <img
+                      src={item.image}
+                      className="img-fluid"
+                      alt={item.title}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
