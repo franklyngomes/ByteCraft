8198fc5ab91a8ef19e6eae609056fc78c13ed091
franklyngomes <franklyngomes15@gmail.com>
2025-05-17 11:23:39 +0530

    Fix: remove submodule and add client as normal folder

---
+++ client/app/components/Header.tsx
@@ -0,0 +1,92 @@
+"use client"
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header id="header" className="fixed-top">
+    <div className="container d-flex align-items-center">
+      <Link href="/" className="logo mr-auto">
+        <Image src="/img/logo.png" alt="Logo" className="img-fluid" width={100} height={65} />
+      </Link>
+
+      <nav className="nav-menu d-none d-lg-block">
+        <ul>
+          <li className="active">
+            <Link href="/">Home</Link>
+          </li>
+
+          <li className="drop-down">
+            <Link href="">About</Link>
+            <ul>
+              <li>
+                <Link href="/about">About Us</Link>
+              </li>
+              <li>
+                <Link href="/team">Team</Link>
+              </li>
+              <li>
+                <Link href="/testimonials">Testimonials</Link>
+              </li>
+              <li className="drop-down">
+                <Link href="#">Deep Drop Down</Link>
+                <ul>
+                  <li>
+                    <Link href="#">Deep Drop Down 1</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Deep Drop Down 2</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Deep Drop Down 3</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Deep Drop Down 4</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Deep Drop Down 5</Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li>
+            <Link href="/services">Services</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link href="/pricing">Pricing</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="header-social-links">
+        <Link href="/" className="twitter">
+          <i className="icofont icofont-twitter"></i>
+        </Link>
+        <Link href="/" className="facebook">
+          <i className="icofont icofont-facebook"></i>
+        </Link>
+        <Link href="/" className="instagram">
+          <i className="icofont icofont-instagram"></i>
+        </Link>
+        <Link href="/" className="linkedin">
+          <i className="icofont icofont-linkedin"></i>
+        </Link>
+      </div>
+    </div>
+    </header>
+  );
+};
+
+export default Header;
